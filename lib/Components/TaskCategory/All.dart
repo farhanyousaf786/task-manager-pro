6bb2670c7372503ca919b2194708f540478c762a
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-12-30 11:59:11 -0500

    setting All Task Page

--- lib/Components/TaskCategory/All.dart
+++ lib/Components/TaskCategory/All.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:taskreminder/Database/TaskModel.dart';
 
 class AllTask extends StatefulWidget {
@@ -30,14 +32,17 @@ class AllTask extends StatefulWidget {
 }
 
 class _AllTaskState extends State<AllTask> {
+  List<String> subList = [];
+  late DateTime myDate;
+  late bool isLoading = true;
+
   @override
   void initState() {
+    initializeDateFormatting('pt_BR', null);
     slipSubTask();
     getTodayAndFutureTask();
     super.initState();
   }
-
-  List<String> subList = [];
 
   slipSubTask() {
     //this will split every subTask from _F_ Flaf
@@ -56,12 +61,28 @@ class _AllTaskState extends State<AllTask> {
     }
   }
 
-  getTodayAndFutureTask(){
-
-
-    print("date: ${widget.date}");
-
+  getTodayAndFutureTask() {
+    // DateTime tempDate = new DateFormat("yyyy-MM-dd").parse(widget.date);
+    var trimmedDate =
+        widget.date.substring(0, widget.date.lastIndexOf(' ') + 1);
+    Future.delayed(const Duration(seconds: 1), () {
+      myDate = Intl.withLocale(
+          'en', () => new DateFormat("yyyy-MM-dd").parse(trimmedDate));
+      print("date: ${myDate}");
+      setState(() {
+        isLoading = false;
+      });
+    });
   }
+
+  /// Returns the difference (in full days) between the provided date and today.
+  int calculateDifference(DateTime date) {
+    DateTime now = DateTime.now();
+    return DateTime(date.year, date.month, date.day)
+        .difference(DateTime(now.year, now.month, now.day))
+        .inDays;
+  }
+
   @override
   Widget build(BuildContext context) {
     var otherTaskCard = TaskModel(
@@ -74,15 +95,98 @@ class _AllTaskState extends State<AllTask> {
       isComplete: widget.isComplete,
     );
 
+    if (isLoading) {
+      return Text("Loading..");
+    } else {
+      return Column(
+        children: [
+          // allTask(),
+
+          if (calculateDifference(myDate) == 0)
+            TodayTask(otherTaskCard)
+          else
+            calculateDifference(myDate) == 1
+                ? TomorrowTasks(otherTaskCard)
+                : calculateDifference(myDate) == -1
+                    ? SizedBox(
+                        height: 0,
+                      )
+                    : FutureTask(otherTaskCard),
+        ],
+      );
+    }
+  }
+
+  TodayTask(var otherTaskCard) {
     return Column(
       children: [
-        const Text("Today"),
-        Column(
-          children: [
-            Text("Task: ${widget.task}"),
-            for (var i in subList) Text(i.toString()),
-          ],
+        Text("Today Tasks"),
+        Text("Task: ${widget.task}"),
+        for (var i in subList) Text(i.toString()),
+        GestureDetector(
+          onTap: () => widget.deleteFunction(otherTaskCard),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.blueAccent.shade200.withOpacity(0.85),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 20, right: 20, bottom: 3, top: 3),
+              child: Text(
+                "Delete",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'bal',
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ),
+      ],
+    );
+  }
+
+  TomorrowTasks(var otherTaskCard) {
+    return Column(
+      children: [
+        Text("Tommorow Tasks"),
+        Text("Task: ${widget.task}"),
+        for (var i in subList) Text(i.toString()),
+        GestureDetector(
+          onTap: () => widget.deleteFunction(otherTaskCard),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.blueAccent.shade200.withOpacity(0.85),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 20, right: 20, bottom: 3, top: 3),
+              child: Text(
+                "Delete",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'bal',
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  FutureTask(var otherTaskCard) {
+    return Column(
+      children: [
+        Text("Future Tasks"),
+        Text("Task: ${widget.task}"),
+        for (var i in subList) Text(i.toString()),
         GestureDetector(
           onTap: () => widget.deleteFunction(otherTaskCard),
           child: Container(
